2a0f90ad1c855576e0b82bccdd22562caed285dd
akavanagh119 <62311450+akavanagh119@users.noreply.github.com>
2020-03-17 22:46:19 +0000

    Add files via upload

---
+++ structure.vb/structure.vb/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOutput = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblSecond = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtSecondName = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnOutput
+        '
+        Me.btnOutput.Location = New System.Drawing.Point(235, 306)
+        Me.btnOutput.Name = "btnOutput"
+        Me.btnOutput.Size = New System.Drawing.Size(75, 58)
+        Me.btnOutput.TabIndex = 0
+        Me.btnOutput.Text = "customer thing"
+        Me.btnOutput.UseVisualStyleBackColor = True
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(46, 48)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(51, 20)
+        Me.lblName.TabIndex = 1
+        Me.lblName.Text = "Name"
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(46, 77)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(86, 20)
+        Me.lblFirstName.TabIndex = 2
+        Me.lblFirstName.Text = "First Name"
+        '
+        'lblSecond
+        '
+        Me.lblSecond.AutoSize = True
+        Me.lblSecond.Location = New System.Drawing.Point(22, 118)
+        Me.lblSecond.Name = "lblSecond"
+        Me.lblSecond.Size = New System.Drawing.Size(110, 20)
+        Me.lblSecond.TabIndex = 3
+        Me.lblSecond.Text = "Second Name"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(46, 160)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(48, 20)
+        Me.lblEmail.TabIndex = 4
+        Me.lblEmail.Text = "Email"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(134, 42)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(393, 26)
+        Me.txtName.TabIndex = 5
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(134, 75)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(393, 26)
+        Me.txtFirstName.TabIndex = 6
+        '
+        'txtSecondName
+        '
+        Me.txtSecondName.Location = New System.Drawing.Point(134, 112)
+        Me.txtSecondName.Name = "txtSecondName"
+        Me.txtSecondName.Size = New System.Drawing.Size(393, 26)
+        Me.txtSecondName.TabIndex = 7
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(134, 161)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(393, 26)
+        Me.txtEmail.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtSecondName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblSecond)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.btnOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOutput As Button
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblSecond As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtSecondName As TextBox
+    Friend WithEvents txtEmail As TextBox
+End Class
---
+++ structure.vb/structure.vb/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+    Private Structure Customer
+        'public variables
+        Public FirstName As String
+        Public LastName As String
+        Public Email As String
+        Public ReadOnly Property Name() As String
+            Get
+                Return FirstName & " " & LastName
+            End Get
+        End Property
+    End Structure
+
+    Private objCustomer As Customer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOutput.Click
+        objCustomer.FirstName = "Jim"
+        objCustomer.LastName = "Johnson"
+        objCustomer.Email = "JimJohnsonsemail"
+
+        DisplayCustomer(objCustomer)
+    End Sub
+
+    Private Sub DisplayCustomer(ByVal cust As Customer)
+        txtName.Text = cust.Name
+        txtFirstName.Text = cust.FirstName
+        txtSecondName.Text = cust.LastName
+        txtEmail.Text = cust.Email
+
+    End Sub
+End Class
