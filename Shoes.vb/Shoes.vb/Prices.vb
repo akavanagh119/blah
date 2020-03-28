3a6af9c288132b0f18053c5282f274df1c9b27d0
akavanagh119 <62311450+akavanagh119@users.noreply.github.com>
2020-03-28 22:01:18 +0000

    Add files via upload

---
+++ Shoes.vb/Shoes.vb/Prices.vb
@@ -0,0 +1,17 @@
+﻿Public Class Prices
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dblClassic As Double = 54.49
+        Dim dblRetro As Double = 49.5
+        Dim dblVintage As Double = 44.99
+        Dim dblQuarter As Double = 8.99
+        Dim dblVamp As Double = 14.99
+        Dim dblEyestay As Double = 5
+        Dim dblHeel As Double = 4.99
+        Dim dblHeelBack As Double = 6.49
+        Dim dblLaces As Double = 4
+        Dim dblLogo As Double = 0.18
+        Dim dblText As Double = 0.45
+        Dim dblAddChar As Double = 0.05
+        Dim dblVat As Double = 0.23
+    End Sub
+End Class
